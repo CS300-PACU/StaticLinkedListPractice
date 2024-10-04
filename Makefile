bdca4bf66655b7b9c658e172fbe3b8b053e30370
chadd williams <will4614@pacificu.edu>
2024-10-04 21:26:13 +0000

    cleanup code

--- Makefile
+++ Makefile
@@ -22,7 +22,10 @@ valgrind: bin/main
 	valgrind -v --leak-check=yes --track-origins=yes --leak-check=full --show-leak-kinds=all bin/main
 
 printMain:
-	enscript -C -T 2 -p - -M Letter -Ec --color -fCourier8 src/main.c  | ps2pdf - bin/main.pdf
+	enscript -C -T 2 -p - -M Letter -Ec --color -fCourier12 src/main.c  | ps2pdf - bin/main.pdf
+
+printMain:
+	enscript -C -T 2 -p - -M Letter -Ec --color -fCourier12 src/main2.c  | ps2pdf - bin/main2.pdf
 
 clean:
 	rm -f bin/main bin/*.o
--- src/main2.c
+++ src/main2.c
@@ -11,6 +11,8 @@ typedef struct List* ListPtr;
 typedef struct List
 {
   ListElementPtr psHead;
+  ListElementPtr psTail;
+  ListElementPtr psCurrent;
 }List;
 
 int main ()
@@ -22,6 +24,8 @@ int main ()
   ListElement sNode1, sNode2, sNode3;
 
   // initialize the list to empty
+  sMylist.psCurrent = NULL;
+  sMylist.psTail = NULL;
   sMyList.psHead = NULL;
 
   // initialize the nodes
@@ -32,7 +36,8 @@ int main ()
   sNode3.data = 50;
   sNode3.psNext = NULL;
 
-  // TODO: place sNode3 into the list
+  // TODO: place sNode3 into the 
+  // psHead and psTail should both point to sNode3
 
 
 
@@ -43,6 +48,7 @@ int main ()
 
   // TODO: place sNode1 into the list so that it is in front of sNode3
   // psHead should point to the first node in the list
+  // psTail should point to the last node in the list
 
 
 
@@ -55,6 +61,7 @@ int main ()
   // TODO: place sNode2 into the list so that it is at the end of
   // the list
   // psHead should point to the first node in the list
+  // psTail should point to the last node in the list
 
 
 
@@ -69,7 +76,7 @@ int main ()
 
 
   // TODO: Print the list's data to the screen, one integer per line
-  // REQUIRED: Declare a new pointer to walk the list
+  // REQUIRED: use sMyList.psCurrent to walk the list
 
 
 
@@ -83,8 +90,8 @@ int main ()
 
 
 
-  // TODO: remove the middle node from the list
-
+  // TODO: remove the last node from the list
+  // update psHead and psTails as appropriate
 
 
 
